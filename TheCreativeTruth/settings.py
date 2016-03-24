aea3f50e3cce2528373bb11d288eeea27add58cd
Carter Fawson <carteraaronfawson@gmail.com>
2016-03-24 13:41:08 -0500

    Just updated the database and static file settings

--- TheCreativeTruth/settings.py
+++ TheCreativeTruth/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'TheCreativeTruth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoappsdb',
+        'USER': 'cfawdev',
+        'PASSWORD': 'Ngaue1992',
+        'HOST': 'djangoappsdb.cym14jm9u5vp.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -102,5 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
