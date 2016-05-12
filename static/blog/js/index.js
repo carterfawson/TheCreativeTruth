3e9910bbcdef0c8d5705d42aaff6651fe3f98c9e
Carter Fawson <carteraaronfawson@gmail.com>
2016-05-11 22:54:27 -0400

    WIP header

--- static/blog/js/index.js
+++ static/blog/js/index.js
@@ -1,80 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-/**
- * This object controls the nav bar. Implement the add and remove
- * action over the elements of the nav bar that we want to change.
- *
- * @type {{flagAdd: boolean, elements: string[], add: Function, remove: Function}}
- */
-var myNavBar = {
-
-    flagAdd: true,
-
-    elements: [],
-
-    init: function (elements) {
-        this.elements = elements;
-    },
-
-    add : function() {
-        if(this.flagAdd) {
-            for(var i=0; i < this.elements.length; i++) {
-                document.getElementById(this.elements[i]).className += " fixed-theme";
-            }
-            this.flagAdd = false;
-        }
-    },
-
-    remove: function() {
-        for(var i=0; i < this.elements.length; i++) {
-            document.getElementById(this.elements[i]).className =
-                    document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
-        }
-        this.flagAdd = true;
-    }
-
-};
-
-/**
- * Init the object. Pass the object the array of elements
- * that we want to change when the scroll goes down
- */
-myNavBar.init(  [
-    "header",
-    "header-container",
-    "brand"
-]);
-
-/**
- * Function that manage the direction
- * of the scroll
- */
-function offSetManager(){
-
-    var yOffset = 0;
-    var currYOffSet = window.pageYOffset;
-
-    if(yOffset < currYOffSet) {
-        myNavBar.add();
-    }
-    else if(currYOffSet == yOffset){
-        myNavBar.remove();
-    }
+    $(".hide-search").hide();
+    $(".nav-drop").click(function() {
+        $(".hide-search").toggle("fast");
+        $(".nav-hide").toggle("slow");
+    })
 
 }
-
-/**
- * bind to the document scroll detection
- */
-window.onscroll = function(e) {
-    offSetManager();
-}
-
-/**
- * We have to do a first detectation of offset because the page
- * could be load with scroll down set.
- */
-offSetManager();
-});/**
- * Created by cfaw on 5/8/16.
- */
